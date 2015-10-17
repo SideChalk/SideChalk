0d7716325ac2fc69e28b9216302d3801a726d122
mochuelo-kun <mochuelokun@gmail.com>
2015-10-17 15:54:30 -0700

    (test) Basic React/Redux Skeleton architecture

--- src/scripts/actions/actions.js
+++ src/scripts/actions/actions.js
@@ -2,12 +2,10 @@ export const ADD_MESSAGE = 'ADD_MESSAGE';
 
 
 // "actions" in redux are just objects describing a type and the change desired (in this case a new message to be added)
-function addMessage (message, location) {
+export function addMessage (message, location) {  //ES6-style module.exports.addMessage = addMessage
   return {
     type: ADD_MESSAGE,
     message,
     location
   };
 };
-
-export addMessage;  //ES6-style module.exports.addMessage = addMessage
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,19 @@
-var React = require('react');
+// var React = require('react');
 
-var App = require('./components/hello.react');
+// var App = require('./components/tinyList');
 
-React.render(
-  <App />,
-  document.getElementById('app')
-);
-
+import React from 'react';
+import { render } from 'react-dom';
 import { createStore } from 'redux';  //ES6-style createStore = require('redux').createStore
+import { Provider } from 'react-redux';
 
-import messages from './reducers/reducers.js';
+
+import App from './components/tinyList.js';
+import { messages } from './reducers/reducers.js';
 import { addMessage } from './actions/actions.js'
 
+console.log("messsages:", messages);
+console.log("addMessage:", addMessage);
 
 let store = createStore(messages);      //ES6-style var declaration
 
@@ -22,9 +24,22 @@ let unsubscribe = store.subscribe(     //redux "subscribe" is a listener for cha
 );
 
 
+
+let rootElement = document.getElementById('app');
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
+
 // redux: store.dispatch is the only way to trigger state changes. takes an action as arg
 store.dispatch(addMessage('testing 123...', [45,65]));
 store.dispatch(addMessage('testing 456...', [10,15]));
+store.dispatch(addMessage('boo yeah!', [1,2]));
+store.dispatch(addMessage('now we are cooking.', [4,88]));
+store.dispatch(addMessage('Sup guys.', [50,50]));
 
 
 //stop listening/logging
