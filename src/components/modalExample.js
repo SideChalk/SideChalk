a416cfc5d247feb4057257cd09db7c594a6c8f65
Victor Algaze <valgaze@gmail.com>
2015-10-21 19:17:08 -0700

    Implement ES6 stuff to match Alex's code

--- src/components/modalExample.js
+++ src/components/modalExample.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Button, Modal} from 'react-bootstrap';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { login } from 'actions/actions.js';
 
-class ModalExample extends React.Component {
-    constructor() {
-        super();
-        this.open = this.open.bind(this);
-        this.close = this.close.bind(this);
-
-        this.render = this.render.bind(this);
-        this.state = {
-          showModal: false,
-          buttonLabel: "Show The Modal"
-        };
+ class ModalExample extends React.Component {
+    static propTypes = {
+      loggingIn: React.propTypes.bool,
+      login: React.propTypes.func
     }
+    // constructor() {
+    //     super();
+    //     //this.open = this.open.bind(this);
+    //     //this.close = this.close.bind(this);
+        
 
+    //     //this.render = this.render.bind(this);
+    //     this.state = {
+    //       showModal: false,
+    //       buttonLabel: "Show The Modal"
+    //     };
+    // }
+    componentDidMount(){
+      this.state = {
+        showModal: false,
+        buttonLabel: "Show The Modal"
+      };
+    }
     close() {
       this.setState({ showModal: false });
     }
@@ -21,23 +34,37 @@ class ModalExample extends React.Component {
     open() {
       this.setState({ showModal: true });
     }
-
   render() {
 
     return (
       <div>
-      <Button bsStyle="primary" bsSize="medium" onClick={this.open}>{this.state.buttonLabel}</Button>
+      <Button bsStyle="primary" bsSize="medium" onClick={() => this.open()}>Open Modal</Button>
       <Modal show={this.state.showModal} onHide={this.close}>
              <Modal.Header closeButton>
-               <Modal.Title>Modal heading</Modal.Title>
+               <Modal.Title>Please Login</Modal.Title>
              </Modal.Header>
              <Modal.Body>
-               <h4>Text in a modal</h4>
-               <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+               <h4>Login with one of the providers below:</h4>
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    
+                  <div>
+                    <img alt="Bootstrap Image Preview" src="https://placeholdit.imgix.net/~text?txtsize=9&txt=FACEBOOK&w=205&h=35" />
+                  </div>
+                  <div>
+                    <img alt="Bootstrap Image Preview" src="https://placeholdit.imgix.net/~text?txtsize=9&txt=TWITTER&w=205&h=35" />
+                  </div>
+                  <div>
+                    <img alt="Bootstrap Image Preview" src="https://placeholdit.imgix.net/~text?txtsize=9&txt=FACEBOOK&w=205&h=35" />
+                  </div>
+                    
+                  </div>
+                </div>
+
 
              </Modal.Body>
              <Modal.Footer>
-               <Button onClick={this.close}>Close</Button>
+               <Button onClick={() => this.close()}>Close</Button>
              </Modal.Footer>
            </Modal>
      
@@ -46,4 +73,11 @@ class ModalExample extends React.Component {
   }
 }
 
-export default ModalExample;
+const mapStateToProps = (state) => ({
+  loggingIn : state.loggingIn
+});
+const mapDispatchToProps = (dispatch) => ({
+  login : bindActionCreators(login, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalExample);
