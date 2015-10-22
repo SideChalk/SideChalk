dcca27338ee7bc9443693b2707ac430a18cb4118
Alex Figueroa <alexfig221@gmail.com>
2015-10-21 17:13:46 -0700

    Add auth actions and moved syncData function to Root containter

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -4,5 +4,14 @@ export default createConstants(
   'RECEIVE_MEMORY',
   'REMOVE_MEMORY',
   'SEND_MEMORY',
-  'SET_USER_LOCATION'
+
+  'SET_USER_LOCATION',
+
+  'LOGIN',
+  'LOGIN_SUCCESS',
+  'LOGIN_FAIL',
+
+  'LOGOUT',
+  'LOGOUT_SUCCESS',
+  'LOGOUT_FAIL'
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import memories from './memories';
 import location from './location';
+import auth from './auth';
 
 export default combineReducers({
   memories,
-  location
+  location,
+  auth
 });
