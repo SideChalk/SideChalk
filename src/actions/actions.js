0eed608bb34b1ecc0e51f87b7ba6f8cddadde5c6
knevalsky <knevalsky@gmail.com>
2015-10-22 17:12:15 -0700

    (feat) Add update location listener. Initial refactor for syncData into setLocation.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,7 @@ import Firebase from 'firebase';
 import GeoFire from 'geofire';
 // import thunk from 'redux-thunk';
 import { FIREBASE_ROOT, MEMORY_PATH, LOCATION_PATH } from 'constants/FirebasePaths.js';
-import { RECEIVE_MEMORY, REMOVE_MEMORY, SEND_MEMORY } from 'constants/ActionTypes.js';
+import { RECEIVE_MEMORY, REMOVE_MEMORY, SEND_MEMORY, SET_USER_LOCATION, UPDATE_USER_LOCATION } from 'constants/ActionTypes.js';
 
 const baseRef = new Firebase(FIREBASE_ROOT);
 const memoriesRef = baseRef.child(MEMORY_PATH);
@@ -10,7 +10,11 @@ const geoRef = baseRef.child(LOCATION_PATH);
 
 const geoFire = new GeoFire(geoRef);
 
+/* Default radius measured in km */
 const defaultRadius = 5000;
+
+/* Default center lat/long is currently Moscone Center */
+// const defaultCenter = [37.783530, -122.402482];
 const defaultCenter = [25, 25];
 
 const geoQuery = geoFire.query({
@@ -38,6 +42,20 @@ function _removeMemory(key) {
   };
 }
 
+function _setLocation(location) {
+  return {
+    type: SET_USER_LOCATION,
+    payload: location
+  };
+}
+
+function _updateLocation(location) {
+  return {
+    type: UPDATE_USER_LOCATION,
+    payload: location
+  };
+}
+
 export function sendMemory(data, loc) {
   // TODO: Validate input before sending, then take out sampleContent
   const newMem = memoriesRef.push({...sampleContent, data});
@@ -55,53 +73,64 @@ export function sendMemory(data, loc) {
 //   };
 // }
 
-// function setLocation () {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       console.log('Coordinates long:' + position.coords.longitude + ' lat:' + position.coords.latitude);
-//       updateLocationListener();
+export function setLocation () {
+  return (dispatch) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        // set geoQuery center
+        dispatch(_setLocation([latitude, longitude]));
+        syncData(dispatch);
+      });
+
+      navigator.geolocation.watchPosition((position) => {
+        // console.log('Updated coordinates long:' + position.coords.longitude + ' lat:' + position.coords.latitude);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        geoQuery.updateCriteria({
+          center: [latitude, longitude],
+          radius: defaultRadius
+        });
+        dispatch(_updateLocation([latitude, longitude]));
+      });
+    } else {
+      // Set location with default
+      dispatch(_setLocation(defaultCenter));
+      syncData(dispatch);
+    }
+  };
+}
+
+function syncData(dispatch) {
+  geoQuery.on('key_entered', (key, location, distance) => {
+    memoriesRef
+    .child(key)
+    .once('value', (snapshot) => {
+      const memory = {...snapshot.val(), key, location, distance};
+      dispatch(_receiveMemory(memory));
+    });
+  });
+
+  geoQuery.on('key_exited', (key) => {
+    dispatch(_removeMemory(key));
+  });
+}
+// export function syncData() {
+//   // TODO: set up user location handler
+//   console.log('syncing');
+//   return (dispatch) => {
+//     geoQuery.on('key_entered', (key, location, distance) => {
+//       memoriesRef
+//       .child(key)
+//       .once('value', (snapshot) => {
+//         const memory = {...snapshot.val(), key, location, distance};
+//         dispatch(_receiveMemory(memory));
+//       });
 //     });
-//   }
+
+//     geoQuery.on('key_exited', (key) => {
+//       dispatch(_removeMemory(key));
+//     });
+//   };
 // }
-
-export function updateLocationListener () {
-  return (dispatch) => {
-    navigator.geolocation.watchPosition((position) => {
-      console.log('Updated coordinates long:' + position.coords.longitude + ' lat:' + position.coords.latitude);
-      geoQuery.updateCriteria({
-        center: [position.coords.latitude, position.coords.longitude],
-        radius: defaultRadius
-      });
-    });
-  };
-}
-
-export function syncData() {
-  // TODO: set up user location handler
-  // if (navigator.geolocation) {
-  //   // navigator.geolocation.getCurrentPosition((position) => {
-  //   //   console.log('Coordinates long:' + position.coords.longitude + ' lat:' + position.coords.latitude);
-  //   // });
-  //   navigator.geolocation.watchPosition((position) => {
-  //     console.log('Updated coordinates long:' + position.coords.longitude + ' lat:' + position.coords.latitude);
-  //     geoQuery.updateCriteria({
-  //       center: [position.coords.latitude, position.coords.longitude]
-  //     });
-  //   });
-  // }
-  console.log('syncing');
-  return (dispatch) => {
-    geoQuery.on('key_entered', (key, location, distance) => {
-      memoriesRef
-      .child(key)
-      .once('value', (snapshot) => {
-        const memory = {...snapshot.val(), key, location, distance};
-        dispatch(_receiveMemory(memory));
-      });
-    });
-
-    geoQuery.on('key_exited', (key) => {
-      dispatch(_removeMemory(key));
-    });
-  };
-}
