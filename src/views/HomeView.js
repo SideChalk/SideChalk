de2e2ac142355183089b44d98ac295aeb472b19a
Victor Algaze <valgaze@gmail.comgit config --global user.email valgaze@gmail.com>
2015-10-23 16:55:45 -0700

    (feat/mainlogin) Implement click button, show login modal

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import MemoryListContainer from 'containers/MemoryListContainer.js';
+import LoginModal from 'components/loginModal.js';
 
 class HomeView extends React.Component {
 
   render () {
     return (
       <div className='container text-center'>
-        <h1>SideChalk</h1>
+         <LoginModal />
+          <h1>SideChalk</h1>
         <MemoryListContainer />
       </div>
     );
