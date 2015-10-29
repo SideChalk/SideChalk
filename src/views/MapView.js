4581984ca5b1bcb51ce586f2cffe13f293d3e5de
Alex Figueroa <alexfig221@gmail.com>
2015-10-29 12:32:08 -0700

    (Feat) Add map view and fix navbar always showing list button as active

--- src/views/MapView.js
+++ src/views/MapView.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import MapContainer from 'containers/MapContainer.js';
+
 class MapView extends React.Component {
 
   render () {
     return (
       <div className='container'>
-      	MAPVIEW
+      	<MapContainer />
       </div>
     );
   }
