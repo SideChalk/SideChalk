e3c030df3466bfd3bc9b51edb06d28d4861e4861
mochuelo-kun <mochuelokun@gmail.com>
2015-10-28 19:54:28 -0700

    (feat) Can find&add music clips as memories

--- src/components/CreateMemoryModal.js
+++ src/components/CreateMemoryModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Modal, Button, Input } from 'react-bootstrap';
+import { Modal, Button, Input, ListGroup, ListGroupItem, Image, Label } from 'react-bootstrap';
+import $ from 'jquery';
 
 export default class CreateMemoryModal extends React.Component {
   static propTypes = {
@@ -9,21 +10,77 @@ export default class CreateMemoryModal extends React.Component {
     sendMemory: React.PropTypes.func
   };
 
+  constructor() {
+    super();
+    this.state = {
+      musicOptions: [],
+      musicChoice: null
+    };
+    this.closeModal = this.closeModal.bind(this);
+    this.submitMemory = this.submitMemory.bind(this);
+    // this.searchForMusic = this.searchForMusic.bind(this);
+    // this.startStreaming = this.startStreaming.bind(this);
+    // this.pause = this.pause.bind(this);
+    // this.play = this.play.bind(this);
+  }
+
   memoryTypes = {
     text: 'Text',
     music: 'Music',
     drawing: 'Drawing'
   };
 
-  handleClick() {
-    const title = this.refs.title.refs.input;
-    const data = this.refs.data.refs.input;
-    const priv = this.refs.priv.refs.input;
-    // alert(priv.checked);
-    this.props.sendMemory({data: data.value, title: title.value, type: 'text'}, priv.checked);
-    title.value = '';
-    data.value = '';
-    priv.checked = false;
+  closeModal() {
+    // this.refs.title.refs.input.value = '';
+    // this.refs.title.refs.data.value = '';
+    // priv.checked = false;
+    this.setState({
+      musicOptions: [],
+      musicChoice: null
+    });
+    this.props.dismissCreateMemory();
+  }
+
+  searchForMusic() {
+    $.ajax({
+      url: 'https://itunes.apple.com/search',
+      data: {
+        media: 'music',
+        entity: 'song',
+        limit: 6,
+        term: this.refs.musicQuery.refs.input.value
+      },
+      type: 'GET',
+      dataType: 'jsonp',
+      crossDomain: true
+    })
+    .done((response) => {
+      this.setState({musicOptions: response.results});
+    })
+    .fail((err) => {
+      console.log('iTunes API FAIL:', err);
+    });
+  }
+
+  startStreaming(musicInfo) {
+    this.setState({musicChoice: musicInfo});
+  }
+
+  submitMemory() {
+    const title = this.refs.title.refs.input.value;
+    const priv = this.refs.priv.refs.input.checked;
+    let data = null;
+
+    if (this.props.modalType === 'text') {
+      data = this.refs.data.refs.input.value;
+    } else if (this.props.modalType === 'music') {
+      data = this.state.musicChoice;
+    } else if (this.props.modalType === 'drawing') {
+      // data = some kind of drawing info
+    }
+
+    this.props.sendMemory({data: data, title: title, type: this.props.modalType}, priv.checked);
+    this.closeModal();
   }
 
   renderContentInput() {
@@ -41,18 +98,47 @@ export default class CreateMemoryModal extends React.Component {
   renderTextInput() {
     return (
       <Input
-        type='text'
+        type='textarea'
+        style={{minHeight: 200}}
         ref='data'
-        placeholder='Memory..'/>
+        placeholder='Tell me, liebling, what is on your mind?' />
     );
   }
 
   renderMusicInput() {
+    const previewUrl = this.state.musicChoice ? this.state.musicChoice.previewUrl : null;
+    const artistName = this.state.musicChoice ? this.state.musicChoice.artistName : null;
+    const trackName = this.state.musicChoice ? this.state.musicChoice.trackName : null;
+
     return (
-      <Input
-        type='text'
-        ref='data'
-        placeholder='Search for music..'/>
+      <div>
+        <Input
+          type='text'
+          ref='musicQuery'
+          placeholder='Stuck in your head?' />
+        <Button onClick={()=>this.searchForMusic()}>Find Matches</Button>
+        <div style={{width: '50%', float: 'right'}}>
+          <h5>Current Selection:</h5>
+          <em>{artistName} -- {trackName}</em>
+          <audio src={previewUrl} autoPlay controls label='Selection Preview'/>
+        </div>
+        <ListGroup>
+        {this.state.musicOptions.map((option) => {
+          if (option.previewUrl && option.isStreamable) {
+            return (
+              <ListGroupItem onClick={()=>this.startStreaming(option)} key={option.trackId}>
+                <Image src={option.artworkUrl30} rounded
+                  style={{marginRight: 5}}/>
+                <Label>{option.artistName} -- {option.trackName}</Label>
+              </ListGroupItem>
+            );
+          } else {
+            return '';
+          }
+        })}
+        </ListGroup>
+
+      </div>
     );
   }
 
@@ -84,30 +170,31 @@ export default class CreateMemoryModal extends React.Component {
 
     return (
       <Modal show={ showModal }
-             onHide={this.props.dismissCreateMemory}>
+             onHide={this.closeModal}>
         <div className="memory-modal">
           <Modal.Header className="memory-modal-title">
             <Modal.Title>New {this.memoryTypes[this.props.modalType]} Memory</Modal.Title>
           </Modal.Header>
           <Modal.Body className="memory-modal-body">
             { this.renderTitleInput() }
-            { this.renderContentInput() }
-            Private:
             <Input
-              className='message-private'
-              type='checkbox'
-              ref='priv'
-              placeholder='Memory..'/>
+                className='message-private'
+                type='checkbox'
+                label='Private?'
+                ref='priv'
+                style={{color: '#BF4E30', textAlign: 'right'}}
+                placeholder='Memory..'/>
+            { this.renderContentInput() }
           </Modal.Body>
           <Modal.Footer className="memory-modal-footer">
             <Button
               className='add-memory-btn'
-              onClick={() => this.handleClick()}>
+              onClick={() => this.submitMemory()}>
               Add
             </Button>
             <Button
               className='close-modal-btn'
-              onClick={this.props.dismissCreateMemory}>
+              onClick={this.closeModal}>
               Close
             </Button>
           </Modal.Footer>
