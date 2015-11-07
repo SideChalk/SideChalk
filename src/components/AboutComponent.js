34a54e67b76c17b9f235dd25437f1fd2b13581bd
Victor Algaze <valgaze@gmail.com>
2015-11-06 17:21:13 -0800

    (feat) Add nice about page

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,71 +1,63 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import hero1 from '../assets/hero1.png';
+import hero2 from '../assets/hero2.png';
 
 export default class About extends React.Component {
 
 
   render () {
     const _bgDiscover = {};
-    _bgDiscover.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/6/65/Yoga4Love_Freedom_Gratitude.jpg")';
-    // _bgDiscover.backgroundColor = '#222';
-    _bgDiscover.backgroundSize = 'cover';
-    _bgDiscover.backgroundRepeat = 'no-repeat';
-    const _bgMemories = {};
-    _bgMemories.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/5/54/Golden_Gate_Bridge_0002.jpg")';
-    // _bgMemories.backgroundColor = '#333';
-    _bgMemories.backgroundSize = 'cover';
-    _bgMemories.backgroundRepeat = 'no-repeat';
-    const _bgView = {};
-    _bgView.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/7/78/Sparkler.JPG")';
-    // _bgView.backgroundColor = '#375a7f';
-    _bgView.backgroundSize = 'cover';
-    _bgView.backgroundRepeat = 'no-repeat';
-    const _bgRedirect = {};
-    _bgRedirect.cursor = 'pointer';
-
+  
     return (
-      <Grid>
-        <Row>
-          <Col lg={12}>
-            <div className='hero' style={_bgDiscover}>
-              <h1 className='hero-title'> Discover your local community. </h1>
-              <span className='hero-content'>
-              </span>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={12}>
-            <div className='hero' style={_bgMemories}>
-              <h1 className='hero-title'> Share your experiences. </h1>
-              <span className='hero-content'>
-              </span>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={12}>
-            <div className='hero' style={_bgView}>
-              <h1 className='hero-title'> Save your memories. Anytime. Anyplace. </h1>
-              <span className='hero-content'>
-              </span>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={12}>
-            <LinkContainer to="/list">
-              <div className='hero' >
-                <h1 className='hero-title' style={_bgRedirect}> Lets start exploring. </h1>
-                <div className='hero-content'>
-                  Well prompt you for your location
-                </div>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-12">
+                <h2>
+                  Welcome to SideChalk
+                </h2>
+                <p>
+                  SideChalk is a realtime memory experience. All you need to is open up your phone and you’ll see a list of memories located nearby.
+
+                   Who wrote them? Unless they tell you, you’ll never know. <p>SideChalk is not a social network. SideChalk is a throwback to the good old days when all you had to do was grab some chalk and share some a thought or an idea on the sidewalk. To get started just login with Twitter/Facebook/Google (we won't ever email you or post on your behalf) and start sharing memories.</p>
+                </p>
               </div>
-            </LinkContainer>
-          </Col>
-        </Row>
-      </Grid>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <img className="img-responsive" src={hero1} />
+              </div>
+              <div className="col-md-6">
+                <p>
+                  SideChalk makes it easy for you to annonymously leave behind memories all over the world. No matter where you are you can leave behind a memory and see the memories of others. You write or draw or even leave behind a clip of music to capture the mood of a time and place.</p>
+
+                <p>
+                What should you post? Whatever you want. Happy, sad, funny, existential angst, whatever is on your mind you can leave behind a memory. (If there's a memory you want to keep for yourself at a particular location-- like the name of your barista--  we added the option for you to mark memories as private.)
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <p>
+                In addition to leaving behind musings and music clips, we also added the ability to draw on a virtual sidewalk. Pick your colors and start drawing whatever you want!
+                </p>
+              </div>
+              <div className="col-md-6">
+                <img className="img-responsive" src={hero2} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row center-blow">
+        <a className="btn" href="http://www.sidechalkapp.com">Let's Get Started »</a>
+        </div>
+
+      </div>
+
     );
   }
 }
